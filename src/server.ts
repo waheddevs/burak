b0844f20ca460e2366bfc394a2aa82eda1351084
waheddevs <abduvohid_gulomov@mail.ru>
2026-05-31 13:13:17 +0900

    feat: express integration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 const dns = require("dns");
+import app from './app';
 
 // ISP/router DNS often blocks MongoDB Atlas SRV lookups
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
@@ -11,5 +12,8 @@ mongoose.connect(process.env.MONGO_URL as string, {})
 .then((data) => {
   console.log('MongoDB connection succeed');
   const PORT = process.env.PORT ?? 3003;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 })
 .catch(err => console.log('ERROR on conection MongoDB: ', err))
