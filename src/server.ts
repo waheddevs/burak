2a08d5443f21abb8d8a5654bcc3aa230651503d3
waheddevs <abduvohid_gulomov@mail.ru>
2026-05-31 12:54:09 +0900

    feat: connect mongodb via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import dotenv from 'dotenv';
 dotenv.config();
+const dns = require("dns");
 
-console.log('PORT:', process.env.PORT);
+// ISP/router DNS often blocks MongoDB Atlas SRV lookups
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-console.log('MONGO_URL:', process.env.MONGO_URL);
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URL as string, {})
+.then((data) => {
+  console.log('MongoDB connection succeed');
+  const PORT = process.env.PORT ?? 3003;
+})
+.catch(err => console.log('ERROR on conection MongoDB: ', err))
