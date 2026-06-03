6af1d622d399aa8503e1dbe77d6705ce62fb9399
waheddevs <abduvohid_gulomov@mail.ru>
2026-06-03 10:49:01 +0900

    feat: create routing and member controllers

---
+++ src/controllers/member.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import { T } from '../libs/common';
+
+const memberController: T = {};
+memberController.goHome = (req: Request, res: Response) => {
+  try {
+    res.send('Home Page!');
+  } catch (err) {
+    console.log('ERROR on goHome: ', err);
+  }
+};
+
+memberController.getLogin = (req: Request, res: Response) => {
+  try {
+    res.send('Login Page!');
+  } catch (err) {
+    console.log('ERROR on getLogin: ', err);
+  }
+};
+
+memberController.getSignup = (req: Request, res: Response) => {
+  try {
+    res.send('Signup Page!');
+  } catch (err) {
+    console.log('ERROR on getSignup: ', err);
+  }
+};
+
+export default memberController;
