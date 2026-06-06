55a18659e0db127f046306dfdfd164f1fd635dc1
waheddevs <abduvohid_gulomov@mail.ru>
2026-06-06 11:57:41 +0900

    feat: develop restaurant login process

--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -14,6 +14,10 @@ export enum Message {
   NO_DATA_FOUND = 'No data found',
   CREATE_FAILED = 'Create failed',
   UPDATE_FAILED = 'Update failed',
+
+  USED_NICK_PHONE = 'You are inserting already used nick or phone!',
+  NO_MEMBER_NICK = 'No member with that member nick!',
+  WRONG_PASSWORD = 'Wrong password, please try again!',
 }
 
 class Errors extends Error {
