5e74ea87d4b3ef21e1e47db3916fafac353db6c5
waheddevs <abduvohid_gulomov@mail.ru>
2026-06-10 08:58:55 +0900

    feat: frontent related css, js and img creation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -10,7 +10,7 @@ const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
   try {
     console.log('goHome');
-    res.send('Home Page!');
+    res.render('home');
     // response => send | json | render | redirect | end
   } catch (err) {
     console.log('ERROR on goHome: ', err);
@@ -20,7 +20,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 restaurantController.getSignup = (req: Request, res: Response) => {
   try {
     console.log('getSignup');
-    res.send('Signup Page!');
+    res.render('signup');
   } catch (err) {
     console.log('ERROR on getSignup: ', err);
   }
@@ -29,7 +29,7 @@ restaurantController.getSignup = (req: Request, res: Response) => {
 restaurantController.getLogin = (req: Request, res: Response) => {
   try {
     console.log('getLogin');
-    res.send('Login Page!');
+    res.render('login');
   } catch (err) {
     console.log('ERROR on getLogin: ', err);
   }
--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ mongoose.connect(process.env.MONGO_URL as string, {})
   const PORT = process.env.PORT ?? 3003;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.info(`Admin project on http://localhost:${PORT}/admin \n`)
   });
 })
 .catch(err => console.log('ERROR on conection MongoDB: ', err))
