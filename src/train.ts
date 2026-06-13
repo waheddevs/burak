6b330dd355013972846d9934558c6b04a30ec78e
waheddevs <abduvohid_gulomov@mail.ru>
2026-06-13 11:40:35 +0900

    feat; Task-R has been done

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,25 @@
+/* R-TASK
+
+Shunday function yozing, u string parametrga ega bolsin. String "1+2" holatda pass qilinganda string ichidagi sonlar yigindisini number holatda qaytarsin.
+MASALAN: calculate("1+3") return 4;
+
+*/
+
+// Masalaning yechimi:
+
+function calculate(expression: string): number {
+    const [a, b] = expression.split("+").map(Number);
+    return a + b;
+}
+
+console.log(calculate("4+5"));
+console.log(calculate("11+35"));
+
+
+
+
+//=========================================================================================================================================
+
 /* TASK Q:
 
 Shunday function yozing, u 2 ta parametrga ega bo'lib birinchisi object, ikkinchisi string bo'lsin. Agar qabul qilinayotgan ikkinchi string, objectning biror bir propertysiga mos kelsa, 'true', aks holda mos kelmasa 'false' qaytarsin.
@@ -16,11 +38,6 @@ function hasProperty(obj: object, key: string): boolean {
 console.log(hasProperty({ name: "MERS", model: "350" }, "model"));
 console.log(hasProperty({ name: "CHEVROLET", model: "COBALT" }, "model"));
 console.log(hasProperty({ age: 25, city: "Jizzakh" }, "age"));
-
-
-
-
-
 
 
 
