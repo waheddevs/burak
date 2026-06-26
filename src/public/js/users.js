3f45963e2eb2078b4e941961ef35128046fc79ee
waheddevs <abduvohid_gulomov@mail.ru>
2026-06-26 20:14:28 +0900

    feat: update choosen user frontedn logic

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,23 +1,33 @@
 console.log("Users frontend javascript file");
 
-document.querySelectorAll('.member-status').forEach(function (select) {
-  select.addEventListener('change', function () {
-    const _id = this.id;
-    const memberStatus = this.value;
+$(function () {
+  $('.member-status').on('change', function(e) {
+    const id = e.target.id;
+    console.log('id:', id)
 
-    fetch('/admin/user/edit', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ _id: _id, memberStatus: memberStatus }),
+    const memberStatus = $(`#${id}.member-status`).val();
+    console.log('memberStatus', memberStatus)
+
+    // TODO axios updateChoosenUser
+    axios.post('/admin/user/edit', {
+      _id: id,
+      memberStatus: memberStatus
     })
-      .then(function (res) {
-        if (!res.ok) {
-          alert('Status update failed!');
+      .then( response =>  {
+        console.log('response:', response);
+        const result = response.data;
+        console.log('result:', result);
+
+        if(result.data) {
+          console.log('User updated!')
+          $('.member-status').blur();
+        } else {
+          alert('User update failed')
         }
       })
-      .catch(function (err) {
-        console.error('Error:', err);
-        alert('Something went wrong!');
+      .catch( err => {
+        console.log(err);
+        alert('User update failed!')
       });
-  });
-});
+  })
+})
