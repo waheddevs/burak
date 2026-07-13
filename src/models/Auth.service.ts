94879d450eac058d2f73c46bd5e84f4a20c4f2f2
waheddevs <abduvohid_gulomov@mail.ru>
2026-07-13 17:52:36 +0900

    feat: create view schema and service models

--- src/models/Auth.service.ts
+++ src/models/Auth.service.ts
@@ -1,7 +1,7 @@
-import { AUTH_TIMER } from "../libs/config";
-import Errors, { HttpCode, Message } from "../libs/Errors";
-import { Member } from "../libs/types/member";
-import jwt from "jsonwebtoken";
+import { AUTH_TIMER } from '../libs/config';
+import Errors, { HttpCode, Message } from '../libs/Errors';
+import { Member } from '../libs/types/member';
+import jwt from 'jsonwebtoken';
 
 class AuthService {
   private readonly secretToken: string;
@@ -13,26 +13,25 @@ class AuthService {
   public async createToken(payload: Member) {
     return new Promise((resolve, reject) => {
       const duration = `${AUTH_TIMER}h`;
-      jwt.sign(payload, process.env.SECRET_TOKEN as string, {
-        expiresIn: duration,
-      }, (err, token) => {
-        if(err) reject(new Errors(HttpCode.UNAUTHORIZED, Message.TOKEN_CREATION_FAILED))
-        else resolve(token as string);
-      });
-    })
+      jwt.sign(
+        payload,
+        process.env.SECRET_TOKEN as string,
+        {
+          expiresIn: duration,
+        },
+        (err, token) => {
+          if (err) reject(new Errors(HttpCode.UNAUTHORIZED, Message.TOKEN_CREATION_FAILED));
+          else resolve(token as string);
+        },
+      );
+    });
   }
 
   public async checkAuth(token: string): Promise<Member> {
-    const result: Member = (await jwt.verify(
-      token,
-      this.secretToken
-    )) as unknown as Member;
-    console.log(`--- [AUTH] memberNick: ${result.memberNick}`)
+    const result: Member = (await jwt.verify(token, this.secretToken)) as unknown as Member;
+    console.log(`--- [AUTH] memberNick: ${result.memberNick}`);
     return result;
   }
-
-
-
 }
 
 export default AuthService;
