83e9e6d17543e61195258b0dc7898e06fb036650
waheddevs <abduvohid_gulomov@mail.ru>
2026-07-15 04:10:57 +0900

    feat: develop createOrder rest api

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import memberController from './controllers/member.controller';
 import uploader from './libs/utlis/uploader';
 import restaurantController from './controllers/restaurant.controller';
 import productController from './controllers/product.controller';
+import orderController from './controllers/order.controller';
 
 /** MEMBER **/
 router.get('/member/restaurant', memberController.getRestaurant);
@@ -25,5 +26,7 @@ router.get('/product/all/', productController.getProducts);
 router.get('/product/:id', memberController.retrieveAuth, productController.getProduct);
 
 /** ORDER **/
+
+router.post('/order/create', memberController.verifyAuth, orderController.createOrder);
 
 export default router;
