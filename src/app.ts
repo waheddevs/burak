2683dc10b6d8bb2a02876aa690e430a02f95e8f9
waheddevs <abduvohid_gulomov@mail.ru>
2026-07-27 19:41:08 +0900

    feat: CORS integration

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
+import cors from 'cors';
 import express from 'express';
 import path from 'path';
 import router from './router';
 import routerAdmin from './router-admin';
 import morgan from 'morgan';
-import cookieParser from "cookie-parser"
+import cookieParser from 'cookie-parser';
 import { MORGAN_FORMAT } from './libs/config';
 
 import session from 'express-session';
@@ -13,15 +14,16 @@ import { T } from './libs/types/common';
 const MongoDBStore = ConnectMongoDB(session);
 const store = new MongoDBStore({
   uri: String(process.env.MONGO_URL),
-  collection: 'sessions'
+  collection: 'sessions',
 });
 
 /** 1-ENTRANCE **/
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
-app.use("/uploads", express.static("./uploads"))
+app.use('/uploads', express.static('./uploads'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors({ credentials: true, origin: true }));
 app.use(cookieParser());
 app.use(morgan(MORGAN_FORMAT));
 
@@ -30,26 +32,25 @@ app.use(
   session({
     secret: String(process.env.SESSION_SECRET),
     cookie: {
-      maxAge: 1000 * 3600 * 6 // 3h
+      maxAge: 1000 * 3600 * 6, // 3h
     },
     store: store,
     resave: true,
-    saveUninitialized: true
-  })
+    saveUninitialized: true,
+  }),
 );
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const sessionInstance = req.session as T;
   res.locals.member = sessionInstance.member;
   next();
-})
-
+});
 
 /** 3-VIEWS **/
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 /** 4-ROUTERS **/
-app.use('/admin', routerAdmin);  // BSSR: EJS
-app.use('/', router);  // SPA: REACT
+app.use('/admin', routerAdmin); // BSSR: EJS
+app.use('/', router); // SPA: REACT
 
 export default app;
